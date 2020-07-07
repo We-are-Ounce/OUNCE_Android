017217b736ff349c4539709f1783b3bce80e340c
kangmin1012 <kangmin1012@naver.com>
2020-07-07 21:49:12 +0900

    #25 complete

--- app/src/main/java/com/sopt/ounce/catregister/ui/CatProfileRegisterFragment.kt
+++ app/src/main/java/com/sopt/ounce/catregister/ui/CatProfileRegisterFragment.kt
@@ -6,14 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.sopt.ounce.R
 import com.sopt.ounce.catregister.CatRegisterActivity
+import gun0912.tedkeyboardobserver.TedKeyboardObserver
 import kotlinx.android.synthetic.main.fragment_cat_profile_register.*
+import kotlinx.android.synthetic.main.fragment_cat_profile_register.view.*
+import kotlinx.android.synthetic.main.fragment_id.view.*
 
 
 class CatProfileRegisterFragment : Fragment() {
     private lateinit var mContext : Context
     private lateinit var v : View
+    private lateinit var mImm : InputMethodManager
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -25,7 +30,9 @@ class CatProfileRegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_cat_profile_register, container, false)
-
+        //키보드 내리기 + 키보드 감지
+        settingMethodManager()
+        observeKeyboard()
 
         return v
     }
@@ -33,10 +40,29 @@ class CatProfileRegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        layout_catprofile.setOnClickListener {
+            mImm.hideSoftInputFromWindow(edt_catprofile_name.windowToken,0)
+        }
+
     }
 
     private fun observeKeyboard(){
+        activity?.let{
+            TedKeyboardObserver(it)
+                .listen { isShow ->
+                    if (!isShow){
+                        v.edt_catprofile_name.clearFocus()
+                        v.edt_catprofile_explain.clearFocus()
+                    }
+
+                }
+        }
+
+    }
+
+    private fun settingMethodManager(){
         val activity = activity as CatRegisterActivity
+        mImm = activity.setImmToFragment()
 
     }
 
