fb7f6e2ff87338389e1ab2423d8b72547d80dc83
kangmin1012 <kangmin1012@naver.com>
2020-07-08 19:01:27 +0900

    #33 Complete

--- app/src/main/java/com/sopt/ounce/main/MainActivity.kt
+++ app/src/main/java/com/sopt/ounce/main/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.sopt.ounce
+package com.sopt.ounce.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sopt.ounce.searchmain.SearchFragment
+import com.sopt.ounce.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
