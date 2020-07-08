06c9ed77d845612ce0afae5370160ce2ce4b9efd
jooyae <jooyae3970@naver.com>
2020-07-08 18:05:22 +0900

    imageSearchCompleteView

---
+++ app/src/main/java/com/sopt/ounce/record/SearchCompleteActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.ounce.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sopt.ounce.R
+
+class SearchCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_complete)
+    }
+}
