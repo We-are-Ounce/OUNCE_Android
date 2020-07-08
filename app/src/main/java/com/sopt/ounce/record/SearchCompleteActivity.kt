451194e365572351aa7c6bea5726905ed69f51a8
jooyae <jooyae3970@naver.com>
2020-07-08 22:24:49 +0900

    CardViewHolder,adapter

--- app/src/main/java/com/sopt/ounce/record/SearchCompleteActivity.kt
+++ app/src/main/java/com/sopt/ounce/record/SearchCompleteActivity.kt
@@ -2,11 +2,17 @@ package com.sopt.ounce.record
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sopt.ounce.R
 
 class SearchCompleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_complete)
+
+        //val myLayoutManager = GridLayoutManager(this,2)
+        GridLayoutManager(this, 2)
+
     }
 }
