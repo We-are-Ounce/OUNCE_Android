25771910da5e196672e32609299e47fc00f99fbe
kangmin1012 <kangmin1012@naver.com>
2020-07-08 21:43:05 +0900

    #34 complete

--- app/src/main/java/com/sopt/ounce/main/MainActivity.kt
+++ app/src/main/java/com/sopt/ounce/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.sopt.ounce.main
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import com.sopt.ounce.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,5 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        bottom_main_appbar.setOnMenuItemClickListener {
+            when (it.itemId){
+                R.id.main_search -> {
+                    Log.d("ClickCallBack","search")
+                }
+            }
+            true
+        }
+
+        bottom_main_appbar.setNavigationOnClickListener {
+            Log.d("ClickCallBack","logo")
+        }
+
+
+
+    }
+
+
+    fun methodManagerToFragment() : InputMethodManager {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE)
+                as InputMethodManager
+        return imm
     }
 }
---
+++ app/src/main/java/com/sopt/ounce/main/ui/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.sopt.ounce.main.ui
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import com.sopt.ounce.R
+import com.sopt.ounce.main.MainActivity
+
+class HomeFragment : Fragment() {
+
+    private lateinit var mContext : Context
+    private lateinit var v : View
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+
+}
