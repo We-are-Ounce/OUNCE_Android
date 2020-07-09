5d23601608c283f941f26df516755e3fea4b0114
jooyae <jooyae3970@naver.com>
2020-07-09 17:12:06 +0900

    itemdecoration

---
+++ app/src/main/java/com/sopt/ounce/record/RecordItemDecoration.kt
@@ -0,0 +1,27 @@
+package com.sopt.ounce.record
+
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RecordItemDecoration: RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.left = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            26F,
+            view.context.resources.displayMetrics
+        ).toInt()
+    }
+}
+
+
+
+
+
