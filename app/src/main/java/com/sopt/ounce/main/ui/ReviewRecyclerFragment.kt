cdffcc300a0dba0c45038f32ecd7f6a09174d61f
kangmin1012 <kangmin1012@naver.com>
2020-07-09 18:14:47 +0900

    set Spinner

--- app/src/main/java/com/sopt/ounce/main/ui/ReviewRecyclerFragment.kt
+++ app/src/main/java/com/sopt/ounce/main/ui/ReviewRecyclerFragment.kt
@@ -1,15 +1,28 @@
 package com.sopt.ounce.main.ui
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.sopt.ounce.R
+import kotlinx.android.synthetic.main.fragment_review_recycler.*
+import android.widget.SpinnerAdapter
 
 class ReviewRecyclerFragment : Fragment() {
 
+    private lateinit var mContext : Context
     private lateinit var v : View
+    private lateinit var mItem :Array<String>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
 
 
     override fun onCreateView(
@@ -18,7 +31,20 @@ class ReviewRecyclerFragment : Fragment() {
     ): View? {
 
         v = inflater.inflate(R.layout.fragment_review_recycler, container, false)
+        mItem = resources.getStringArray(R.array.main_review_array)
         return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val spinnerAdapter = ArrayAdapter(mContext,
+            R.layout.main_custom_spinner, mItem)
+
+        spinnerAdapter.setDropDownViewResource(R.layout.main_custom_dropdown)
+        spinner_main.apply {
+            adapter = spinnerAdapter
+            }
     }
 
 }
