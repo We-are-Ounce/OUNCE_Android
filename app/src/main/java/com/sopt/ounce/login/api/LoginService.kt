af2a68f6e6ab08f600a57a84be6dc55cbf05be0a
Kangmingu <kangmin1012>
2020-07-14 05:14:48 +0900

    회원가입 통신 완료

--- app/src/main/java/com/sopt/ounce/login/api/LoginService.kt
+++ app/src/main/java/com/sopt/ounce/login/api/LoginService.kt
@@ -2,6 +2,9 @@ package com.sopt.ounce.login.api
 
 import com.sopt.ounce.login.data.RequestLoginData
 import com.sopt.ounce.login.data.ResponseLoginData
+import com.sopt.ounce.signup.data.RequestSignUpdata
+import com.sopt.ounce.signup.data.ResponseSignUpData
+import com.sopt.ounce.signup.data.UserInfoObject
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,4 +14,9 @@ interface LoginService {
     fun postSignIn(
         @Body body : RequestLoginData
     ) : Call<ResponseLoginData>
+
+    @POST("user/signup")
+    fun postSignUp(
+        @Body body : RequestSignUpdata
+    ) : Call<ResponseSignUpData>
 }
