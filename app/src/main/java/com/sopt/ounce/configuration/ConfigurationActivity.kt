a0202c92e03faf3cf79dd3ae64bb2094002401c1
Kangmingu <kangmin1012@naver.com>
2020-07-17 15:25:06 +0900

    설정 창 ui 구성 완료 + 로그아웃 완료

--- app/src/main/java/com/sopt/ounce/configuration/ConfigurationActivity.kt
+++ app/src/main/java/com/sopt/ounce/configuration/ConfigurationActivity.kt
@@ -1,9 +1,13 @@
 package com.sopt.ounce.configuration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amn.easysharedpreferences.EasySharedPreference
 import com.sopt.ounce.R
+import com.sopt.ounce.login.ui.LoginActivity
 import com.sopt.ounce.util.StatusObject
+import com.sopt.ounce.util.showToast
 import kotlinx.android.synthetic.main.activity_configuration.*
 
 class ConfigurationActivity : AppCompatActivity() {
@@ -12,5 +16,45 @@ class ConfigurationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_configuration)
         StatusObject.setStatusBar(this)
 
+        img_configu_back.setOnClickListener {
+            finish()
+        }
+
+        txt_configu_logout.setOnClickListener {
+            EasySharedPreference.Companion.putString("accessToken","")
+            val intent = Intent(this, LoginActivity::class.java)
+            finish()
+            startActivity(intent)
+        }
+
+        txt_configu_info.setOnClickListener {
+            this.showToast("아직 준비 중입니다.")
+        }
+
+        txt_configu_terms.setOnClickListener {
+            this.showToast("아직 준비 중입니다.")
+        }
+
+        txt_configu_version.setOnClickListener {
+            this.showToast("아직 준비 중입니다.")
+        }
+
+        txt_configu_faq.setOnClickListener {
+            this.showToast("아직 준비 중입니다.")
+        }
+
+        txt_configu_report.setOnClickListener {
+            this.showToast("아직 준비 중입니다.")
+        }
+
+        txt_configu_delete.setOnClickListener {
+            this.showToast("아직 준비 중입니다.")
+        }
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
